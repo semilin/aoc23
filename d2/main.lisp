31f2ae58edf1e4f601e7e89505af57ce513ae0df
semi <semilin@disroot.org>
2023-12-02 20:37:42 -0800

    more day 2 improvements

--- d2/main.lisp
+++ d2/main.lisp
@@ -1,9 +1,7 @@
 (defpackage :aoc
   (:use :cl :alexandria :iterate)
   (:import-from :serapeum #:~> #:~>>))
-
-(defparameter *input*
-  (uiop:read-file-lines "input"))
+(in-package :aoc)
 
 ;;; sample input
 ;; (defparameter *input*
@@ -13,51 +11,45 @@
 ;; Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 ;; Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"))
 
+(defparameter *input*
+  (uiop:read-file-lines "input"))
+
+(defun parse-game (str)
+  (let* ((words (serapeum:words str))
+	 (game-id (parse-integer (second words))))
+    (list game-id
+	  (~> words
+	      (subseq 2)
+	      (serapeum:batches 2)
+	      (mapcar (lambda (p)
+			(list (parse-integer (first p))
+			      (second p)))
+		      _)))))
+
 ;; part 1
 (let ((max-counts (serapeum:dict "red" 12 "green" 13 "blue" 14)))
   (~>> *input*
        (mapcar (lambda (line)
-		 (let* ((words (str:words line))
-			(game-id (parse-integer (car (str:split ":" (second words)))))
-			(games (~>> (str:split ";" (str:unwords (subseq words 2)))
-				    (mapcar (lambda (g)
-					      (~>> g (str:split ",")
-						   (mapcar (lambda (pair)
-							     (let ((pair (str:words pair)))
-							       (list (parse-integer (first pair))
-								     (second pair))))))))))
-			(impossible nil))
-
-		   (iterate top (for game in games)
-		     (iterate (for (number color) in game)
-		       (if (> number (gethash color max-counts))
-			   (progn (setf impossible t)
-				  (in top (leave))))))
-		   (if (not impossible)
-		       game-id
-		       0))))
+		 (let* ((game (parse-game line)))
+		   (~> (iterate (for (number color) in (cadr game))
+			 (if (> number (gethash color max-counts))
+			     (leave t)))
+		       (if _
+			   0
+			   (car game))))))
        (apply #'+)
        (print)))
 
 ;; part 2
 (~>> *input*
      (mapcar (lambda (line)
-	       (let* ((words (str:words line))
-		      (games (~>> (str:split ";" (str:unwords (subseq words 2)))
-				  (mapcar (lambda (g)
-					    (~>> g (str:split ",")
-						 (mapcar (lambda (pair)
-							   (let ((pair (str:words pair)))
-							     (list (parse-integer (car pair))
-								   (second pair))))))))))
+	       (let* ((game (parse-game line))
 		      (greatest (serapeum:dict "red" 0 "green" 0 "blue" 0)))
-
-		 (iterate top (for game in games)
-		   (iterate
-		     (for (number color) in game)
-		     (let ((color-count (gethash color greatest)))
-		       (if (> number color-count)
-			   (setf (gethash color greatest) number)))))
+		 (iterate
+		   (for (number color) in (cadr game))
+		   (let ((color-count (gethash color greatest)))
+		     (if (> number color-count)
+			 (setf (gethash color greatest) number))))
 		 (apply #'* (hash-table-values greatest)))))
      (apply #'+)
      (print))
